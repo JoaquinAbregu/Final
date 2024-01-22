313db2eb266fccbf96ce33b3e200b24e881235a3
Joaquin <joaquiea@gmail.com>
2024-01-22 13:38:28 -0300

    Proyecto Final

---
+++ proyecto-final/src/componentes/Cerrar.jsx
@@ -0,0 +1,19 @@
+const Cerrar = () => {
+
+
+    const cerrarSesion = () => {
+        //Eliminamos la sesion
+        window.sessionStorage.removeItem('usuario')
+    }
+
+
+    return(
+        <button 
+        className="btn btn-outline-danger" 
+        onClick={cerrarSesion}
+        >Cerrar Sesion
+        </button>
+    )
+}
+
+export default Cerrar;
